fcc19d0f8e927b4399d3c1d7c0becf6387fc74a3
Bernhard Huemer <bernhard.huemer@gmail.com>
2014-04-27 23:33:43 +0200

    Refactoring code slightly

--- github-future-monad/src/main/scala/at/bhuemer/scala/playground/github/service/HttpGitHubService.scala
+++ github-future-monad/src/main/scala/at/bhuemer/scala/playground/github/service/HttpGitHubService.scala
@@ -12,22 +12,22 @@ class HttpGitHubService[Context[_]: Monad](httpRequestor: HttpRequestor[Context]
   import at.bhuemer.scala.playground.github.monad.syntax._
 
   override def repositoryNamesFor(owner: String): Context[Option[List[String]]] =
-    request(s"/users/$owner/repos") map
-      processResponse { repository: Map[String, String] =>
+    request(s"/users/$owner/repos") {
+      repository: Map[String, String] =>
         // Just extract the name of each repository
         repository.getOrElse("name", "")
       }
 
   override def followerNamesFor(user: String): Context[Option[List[String]]] =
-    request(s"/users/$user/followers") map
-      processResponse { follower: Map[String, String] =>
+    request(s"/users/$user/followers") {
+      follower: Map[String, String] =>
         // Just extract the name (login) of each follower
         follower.getOrElse("login", "")
       }
 
   override def commitsFor(owner: String, repository: String): Context[Option[List[Commit]]] =
-    request(s"/repos/$owner/$repository/commits") map
-      processResponse { commitWithExtras: Map[String, Map[String, Any]] =>
+    request(s"/repos/$owner/$repository/commits") {
+      commitWithExtras: Map[String, Map[String, Any]] =>
         val commit = commitWithExtras("commit")
         val committer =
           commit("committer").asInstanceOf[Map[String, String]]
@@ -39,24 +39,23 @@ class HttpGitHubService[Context[_]: Monad](httpRequestor: HttpRequestor[Context]
         )
       }
 
-  /** Just extracts the URL for the GitHub API */
-  @inline
-  private def request(method: String): Context[Option[String]] =
-    httpRequestor.request("https://api.github.com" + method)
-
   /**
    * Takes care of some of the boiler-plate code involved in parsing these raw responses with the builtin JSON parser.
-   * Will be partially applied so that we can pass it to map().
+   * It also assumes that the request will return a list of something (list of repositories, list of followers, etc.),
+   * but then .. I haven't found anything in the GitHub API that doesn't return a list.
    */
-  private def processResponse[A, B](body: A => B)(maybeRawResponse: Option[String]): Option[List[B]] = {
+  private def request[A, B](method: String)(body: A => B): Context[Option[List[B]]] = {
     import scala.util.parsing.json.JSON
-    for {
-      rawResponse <- maybeRawResponse
-      jsonResponse <- JSON.parseFull(rawResponse)
-      typedResponse <- Some(
-        jsonResponse.asInstanceOf[List[A]]
-      )
-    } yield typedResponse map body
+    httpRequestor.request("https://api.github.com" + method) map {
+      maybeRawResponse =>
+        for {
+          rawResponse <- maybeRawResponse
+          jsonResponse <- JSON.parseFull(rawResponse)
+          typedResponse <- Some(
+            jsonResponse.asInstanceOf[List[A]]
+          )
+        } yield typedResponse map body
+      }
   }
 
 }
